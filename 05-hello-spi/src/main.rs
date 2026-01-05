57254ba8384617e2edb68b5640bddd94b09dbc29
wuhanstudio <wuhanstudios@gmail.com>
2026-01-05 21:57:37 +0000

    [hello-spi] Replace stm32f1xx_hal with embassy_stm32_hal

--- 05-hello-spi/src/main.rs
+++ 05-hello-spi/src/main.rs
@@ -1,21 +1,23 @@
 #![no_main]
 #![no_std]
 
-use panic_halt as _;
 use cortex_m_rt::entry;
-use stm32f1xx_hal::prelude::*;
+use panic_halt as _;
 
-use hal::i2c::I2c;
-use stm32f1xx_hal as hal;
+use embassy_stm32::Config;
+use embassy_stm32::time::Hertz;
+
+use ssd1306::I2CDisplayInterface;
+// use embassy_stm32::{gpio, spi::Spi};
 
 // SPI
-// MOSI - P0.13
-// MISO - P0.01
-// SCK  - P0.17
-// CS   - P1.02
+// MOSI - PA7
+// MISO - PA6
+// SCK  - PA5
+// CS   - PA4
 
-// DC   - P0.09
-// RST  - P0.10
+// DC   - PA3
+// RST  - PA2
 
 // Debugging via RTT, no serrial port needed
 use rtt_target::rtt_init_print;
@@ -28,36 +30,59 @@ use embedded_graphics::{
     text::{Baseline, Text},
 };
 use ssd1306::{Ssd1306, prelude::*};
-use ssd1306::I2CDisplayInterface;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
-    // Get access to the device specific peripherals from the peripheral access crate
-    let dp = stm32f1xx_hal::pac::Peripherals::take().unwrap();
+    // Configure and initialize the embassy STM32 HAL
+    let mut config: Config = Default::default();
+    config.rcc.hse = Some(embassy_stm32::rcc::Hse {
+        freq: Hertz::mhz(8),
+        mode: embassy_stm32::rcc::HseMode::Oscillator,
+    });
 
-    // Acquire the GPIOC peripheral
-    let mut rcc = dp.RCC.constrain();
+    config.rcc.sys = embassy_stm32::rcc::Sysclk::PLL1_P;
+    config.rcc.pll = Some(embassy_stm32::rcc::Pll {
+        src: embassy_stm32::rcc::PllSource::HSE,
+        prediv: embassy_stm32::rcc::PllPreDiv::DIV1,
+        mul: embassy_stm32::rcc::PllMul::MUL9, // 8 * 9 = 72Mhz
+    });
 
-    let gpiob = dp.GPIOB.split(&mut rcc);
+    // Scale down to 36Mhz (maximum allowed)
+    config.rcc.apb1_pre = embassy_stm32::rcc::APBPrescaler::DIV2;
+    let p = embassy_stm32::init(config);
 
-    // Configure I2C1
-    let scl = gpiob.pb6;
-    let sda = gpiob.pb7;
-
-    let i2c = I2c::new(
-        dp.I2C1,
-        (scl, sda),
-        hal::i2c::Mode::standard(100.kHz()),
-        &mut rcc,
+    // Set up the I2C interface
+    let i2c = embassy_stm32::i2c::I2c::new_blocking(
+        p.I2C1,
+        p.PB6,
+        p.PB7,
+        Default::default(),
     );
 
-    // Initialize the display
     let interface = I2CDisplayInterface::new(i2c);
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
 
+    // Set up the SPI interface
+    // let spi = Spi::new_txonly(p.SPI1, p.PA5, p.PA7, p.DMA1_CH3, Default::default());
+    // let cs = gpio::Output::new(p.PA4, gpio::Level::Low, gpio::Speed::Low);
+    // let spi = embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+
+    // let mut rst = gpio::Output::new(p.PA2, gpio::Level::Low, gpio::Speed::Low);
+    // let dc = gpio::Output::new(p.PA3, gpio::Level::Low, gpio::Speed::Low);
+
+    // // Initialize the display
+    // let interface = SPIInterface::new(spi, dc);
+    // let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+    //     .into_buffered_graphics_mode();
+
+    // display
+    //     .reset(&mut rst, &mut embassy_time::Delay {})
+    //     .unwrap();
+
+    // Initialize the display
     display.init().unwrap();
 
     let text_style = MonoTextStyleBuilder::new()
